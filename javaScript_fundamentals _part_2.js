27c79e6026dd712c867aae3c469ccd0337d4fc25
yuvraj bagale <yuvrajbagale123@gmail.com>
2023-11-19 00:06:38 +0530

    updated js code

--- javaScript_fundamentals _part_2.js
+++ javaScript_fundamentals _part_2.js
@@ -92,31 +92,65 @@ console.log(age1, age2);
 // checkWinner(scoreDolphins, scorekoalas)
 
 // Array
-const frineds = ['yuvi', 'sid', 'rushi']
-console.log(frineds);
+// const frineds = ['yuvi', 'sid', 'rushi']
+// console.log(frineds);
 
-const years = new Array(1994, 1999, 1998);
-console.log(years);
-console.log(frineds[0]);
-console.log(frineds[2]);
-console.log(frineds.length);
-console.log(frineds[frineds.length - 1]);
-frineds[2] = "rushikesh"
-console.log(frineds);
+// const years = new Array(1994, 1999, 1998);
+// console.log(years);
+// console.log(frineds[0]);
+// console.log(frineds[2]);
+// console.log(frineds.length);
+// console.log(frineds[frineds.length - 1]);
+// frineds[2] = "rushikesh"
+// console.log(frineds);
 
-const firstName = 'yuvi';
-const yuvi = [firstName, 'Bagale', 2023 - 1994, 'Programmer', frineds];
-console.log(yuvi);
-console.log(yuvi[4][3])
+// const firstName = 'yuvi';
+// const yuvi = [firstName, 'Bagale', 2023 - 1994, 'Programmer', frineds];
+// console.log(yuvi);
+// console.log(yuvi[4][3])
 
 //Exercise
 
-const calcAge = function (birthYear) {
-    return 2023 - birthYear;
+// const calcAge = function (birthYear) {
+//     return 2023 - birthYear;
+// }
+// const age1 = calcAge(years[0]);
+// const age2 = calcAge(years[1]);
+// const age3 = calcAge(years[years.length - 1]);
+// console.log(age1, age2, age3);
+// let ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
+// console.log(ages)
+
+//Basic methods of Array
+const frineds = ['Michael', 'steven', 'Peter']
+
+//Add element
+const newLenght = frineds.push('jay')
+console.log(frineds);
+console.log(newLenght);
+
+frineds.unshift('jay');
+console.log(frineds);
+
+// remove elements
+frineds.pop();//last
+const popped = frineds.pop();
+console.log(popped);
+console.log(frineds);
+
+//remove frist element in the array
+frineds.shift();
+console.log(frineds);
+
+//which position in the array
+console.log(frineds.indexOf('steven'));
+
+//ES6 element includes
+frineds.push(23)
+console.log(frineds.includes('steven'));
+console.log(frineds.includes('Bob'));
+console.log(frineds.includes(23));
+
+if (frineds.includes('steven')) {
+    console.log(`frined name is  ${frineds[0]}`)
 }
-const age1 = calcAge(years[0]);
-const age2 = calcAge(years[1]);
-const age3 = calcAge(years[years.length - 1]);
-console.log(age1, age2, age3);
-let ages = [calcAge(years[0]), calcAge(years[1]), calcAge(years[years.length - 1])]
-console.log(ages)
