0313a9887dc05af4b983ab610a649e5aa90050e3
Yuvraj Bagale <58872409+yuvrajbagale@users.noreply.github.com>
2024-02-18 09:56:46 +0000

    Coversion of dataTypes

---
+++ 01_basic/conversionOperation.js
@@ -0,0 +1,28 @@
+"use strict"
+// let myScore = "true"
+// console.log(typeof myScore);
+// console.log(typeof (myScore));
+
+// let valueInNumber = Number(myScore);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "44" => 44
+// "44abc" => NAN not a number
+// true =>1; false =>0
+
+let isLoggedIn= 1;
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+console.log(booleanIsLoggedIn);
+
+// 1=> true; 0=> false
+// "" => false
+// "hitesh" => true;
+
+let someNumber = 44
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
